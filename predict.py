27cea60eae394599f5341cbccc551b8d188e168a
Bilal Altundağ <50177921+BilalAltundag@users.noreply.github.com>
2022-12-28 12:56:32 +0300

    Add files via upload

---
+++ predict.py
@@ -0,0 +1,31 @@
+import os
+import pickle
+import sys, os
+sys.path.append('D:/Downloads/Fashion_Analysis/Fashion_Analysis/detectron2')
+sys.path.append('D:/Downloads/Fashion_Analysis/Fashion_Analysis/detectron2/detectron2')
+os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
+from detectron2.engine import DefaultPredictor
+from detectron2.data import MetadataCatalog
+
+from utils import *
+
+
+main_dir = os.path.dirname(os.path.realpath(__file__))
+data_dir = os.path.join(main_dir,"data") # Tahmin edilecek resimin bulunduğu klasör
+save_dir = os.path.join(main_dir,"results") # Tahmin sonuçlarının kaydedileceği klasör
+models_dir = os.path.join(main_dir,"modeller") # Model dosyalarının bulunduğu klasör
+
+cfg_path = os.path.join(models_dir, "SE_CFG_3.pickle") # Modelin pickle dosyasının ismi
+model_path = os.path.join(models_dir, "model_final.pth") # Model dosyasının ismi
+
+cfg = get_cfg(cfg_path,model_path)
+predictor = DefaultPredictor(cfg)
+
+def main():
+    print(main_dir)
+    print(data_dir)
+    image_path = os.path.join(data_dir, "example.jpg") # Tahmin edilecek resmin ismi
+    predict_image(image_path, predictor, cfg,save_dir=save_dir, show=True)
+
+if __name__ == '__main__':
+    main()
